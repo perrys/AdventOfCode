8fe067a3e7f39435018a7494bfe92a846cce297b
Stewart Perry <stewart.c.perry@gmail.com>
2023-08-09 07:41:54 +0100

    Implemented parts 1&2 with timer

--- 2022/rust/src/bin/day01.rs
+++ 2022/rust/src/bin/day01.rs
@@ -7,14 +7,54 @@ fn main() {
         panic!("USAGE: {} <filename>", args[0]);
     }
     let contents = read_file(&args[1]);
-    let mut answer: u32 = 0;
-    const N_ITERS: u32 = 10000;
+    let mut largest: u32 = 0;
+    let part1 = || {
+        let mut answer: u32 = 0;
+        let mut max_cb = |result| {
+            if result > answer {
+                answer = result;
+            }
+        };
+        parse_file(&contents, &mut max_cb);
+        largest = answer;
+    };
+    timer(part1);
+    println!("Part1 answer is {largest}");
+    let mut largest3: [u32; 3] = [0; 3];
+    let part2 = || {
+        let mut top3: [u32; 3] = [0; 3];
+        let mut t3 = |result| {
+            if result <= top3[0] {
+                return;
+            }
+            top3[0] = result;
+            if result <= top3[1] {
+                return;
+            }
+            top3[0] = top3[1];
+            top3[1] = result;
+            if result <= top3[2] {
+                return;
+            }
+            top3[1] = top3[2];
+            top3[2] = result;
+        };
+        parse_file(&contents, &mut t3);
+        largest3 = top3;
+    };
+    timer(part2);
+    let sum3: u32 = largest3.iter().sum();
+    println!("Part2 answer is {sum3}");
+}
+
+fn timer<F: FnMut()>(mut func: F) {
+    const N_ITERS: u32 = 1000;
     let mut sum: u128 = 0;
     let mut sumsq: u128 = 0;
     let mut opt_min_dur: Option<u128> = None;
     for _ in 0..N_ITERS {
         let now = Instant::now();
-        answer = find_largest(&contents);
+        func();
         let duration = now.elapsed().as_nanos();
         sum += duration;
         sumsq += duration * duration;
@@ -27,28 +67,23 @@ fn main() {
             None => opt_min_dur = Some(duration),
         }
     }
-    println!("The largest is {answer}");
     let avg = sum as f64 / (N_ITERS as f64);
     let stddev = ((sumsq as f64 / (N_ITERS as f64)) - (avg * avg)).sqrt();
     let min_time = opt_min_dur.unwrap();
     println!("Min time was {min_time:}ns, avg = {avg:.0}+/-{stddev:.0}");
 }
 
-fn find_largest(contents: &str) -> u32 {
-    let mut max: u32 = 0;
+fn parse_file<F: FnMut(u32)>(contents: &str, mut cb: F) {
     let mut sum: u32 = 0;
-    for line in contents.split("\n") {
-        if line.len() > 0 {
+    for line in contents.split('\n') {
+        if !line.is_empty() {
             let val = line.parse::<u32>().unwrap();
             sum += val;
         } else {
-            if 0 == max || sum > max {
-                max = sum;
-            }
+            cb(sum);
             sum = 0;
         }
     }
-    return max;
 }
 
 fn read_file(filename: &str) -> String {
