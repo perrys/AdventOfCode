5b0e82ed241543bddfb01b3c5f1b7d6bcd61f5ba
Stu Perry <stewart.c.perry@gmail.com>
2023-08-25 00:56:44 +0100

    Aesthetics - update comment style

--- 2022/asm/src/common.s
+++ 2022/asm/src/common.s
@@ -1,43 +1,42 @@
-        .section .rodata
-.input_err: .asciz "ERROR: Unable to read input\n"
 
-        .section .data
-.t0:    .quad 0,0           
-.t1:    .quad 0,0           
-.stats: .quad 0,0,0,0
         .equ SUM, 0
         .equ SUMSQ, 8
         .equ COUNT, 16
         .equ MIN, 24
 
+        .section .rodata
+outmsg: .asciz "Elapsed time- min: %dns, avg: %.0f, stddev: %.0f\n"
+input_err: .asciz "ERROR: Unable to read input\n"
+
+        .section .data
+.t0:    .quad 0,0           
+.t1:    .quad 0,0           
+.stats: .quad 0,0,0,0
+
         .section .text
         
-/**
-  Return the absolute value of a (signed) 64-bit integer
-  using abs(x) = (x XOR y) - y, where y = x >> 63
-    %rdi - the value
-*/
+### Return the absolute value of a (signed) 64-bit integer
+### using abs(x) = (x XOR y) - y, where y = x >> 63
+###   %rdi - the value
         .type abs, function
         .global abs
 abs:
-        mov %rdi, %rsi /* rdi = x, rsi will be y */
-        sar $63, %rsi   /* y = all 1s if rdi was negative, all zeros if positive */
-        xor %rsi, %rdi /* let rdi = rdi xor rsi */
-        sub %rsi, %rdi /* let rdi = rdi - rsi */
+        mov %rdi, %rsi  # rdi = x, rsi will be y 
+        sar $63, %rsi   # y = all 1s if rdi was negative, all zeros if positive 
+        xor %rsi, %rdi  # let rdi = rdi xor rsi 
+        sub %rsi, %rdi  # let rdi = rdi - rsi 
         mov %rdi, %rax
         ret
 
-/**
-  Read the entire contents of a file into a buffer. Abort if read fails.
-    rdi - file descriptor
-    rsi - pointer to buffer
-    rdx - size of buffer
-*/
+### Read the entire contents of a file into a buffer. Abort if read fails.
+###   %rdi - file descriptor
+###   %rsi - pointer to buffer
+###   %rdx - size of buffer
         .type read_file, function
         .global read_file
 read_file:
         .equ ST_BYTESREAD, -1*8
-        .equ STSIZE, 1*8 /* keep this 16-byte aligned when calling C functions */
+        .equ STSIZE, 1*8 # keep this 16-byte aligned when calling C functions 
         push %rbp
         mov %rsp, %rbp
         sub $STSIZE, %rsp
@@ -51,7 +50,7 @@ read_file:
         jg .read_success
         .equ FD_STDERR, 1
         mov $FD_STDERR, %rdi
-        mov $.input_err, %rsi
+        mov $input_err, %rsi
         mov $0, %rax
         call fprintf
         mov $1, %rax
@@ -64,11 +63,9 @@ read_file:
         ret
 
         
-/**
-  Call a function in a loop to time it repteatedly, and print out its performance
-   rdi - callback function
-   rsi - number of times to loop
-*/
+### Call a function in a loop to time it repteatedly, and print out its performance
+###  %rdi - callback function
+###  %rsi - number of times to loop
         .type perf_timer, function
         .global perf_timer
 perf_timer:     
@@ -91,7 +88,7 @@ perf_timer:
         mov $0xffffffffffffffff, %rax
         mov %rax, .stats+MIN
         
- .l0:   mov ST_COUNTER(%rbp), %r15   /* loop counter to decrement */
+ .l0:   mov ST_COUNTER(%rbp), %r15 # loop counter to decrement 
         cmp $0, %r15
         je .l0d
 
@@ -126,11 +123,9 @@ perf_timer:
         ret
         
 
-/** 
-  Calculate elapsed time and update statistics
-   rdi - pointer to T0 struct
-   rsi - pointer to T1 struct
-*/
+### Calculate elapsed time and update statistics
+###  %rdi - pointer to T0 struct
+###  %rsi - pointer to T1 struct
         .type mark_time, function
 mark_time:
         call calc_elapsed
@@ -151,20 +146,18 @@ mark_time:
         mov %rax, .stats+COUNT
         ret
 
-
-/**
-  Calculate the elapsed time between to timer points in nanoseconds
-    rdi - pointer to T0 struct
-    rsi - pointer to T1 struct
-*/
+### Calculate the elapsed time between to timer points in nanoseconds
+###   rdi - pointer to T0 struct
+###   rsi - pointer to T1 struct
         .type calc_elapsed, function
 calc_elapsed:
         mov (%rsi), %rax 
-        sub (%rdi), %rax /* subtract the seconds part */
+        sub (%rdi), %rax        # subtract the seconds part 
         .equ NS_PER_SEC, 1000000000
         mov $NS_PER_SEC, %rcx
         mul %rcx
-        /* now offset by the nanoseconds */
+        ## now offset by the nanoseconds
+         
         add 8(%rsi), %rax
         sub 8(%rdi), %rax
         ret
@@ -177,16 +170,14 @@ print_stats:
         movsd .stats+SUM, %xmm0
         mov .stats+COUNT, %rax
         cvtsi2sd %rax, %xmm3
-        divsd %xmm3, %xmm0 /* xmm0 has average */
+        divsd %xmm3, %xmm0      # xmm0 has average
         movsd .stats+SUMSQ, %xmm1
-        divsd %xmm3, %xmm1 /* xmm1 has sumsq/N */
+        divsd %xmm3, %xmm1      # xmm1 has sumsq/N 
         movsd %xmm0, %xmm3
-        mulsd %xmm0, %xmm3 /* xmm3 - avg*avg */
-        subsd %xmm3, %xmm1 /* xmm2 - Var = sumsq/N - avg^2 */
-        sqrtsd %xmm1, %xmm1       /* xmm2 - stddev = sqrt(Var) */
+        mulsd %xmm0, %xmm3      # xmm3 - avg*avg
+        subsd %xmm3, %xmm1      # xmm2 - Var = sumsq/N - avg^2 
+        sqrtsd %xmm1, %xmm1     # xmm2 - stddev = sqrt(Var) 
         mov $2, %rax
         call printf
         ret
 
-        .section .rodata
-outmsg: .asciz "Elapsed time- min: %dns, avg: %.0f, stddev: %.0f\n"
