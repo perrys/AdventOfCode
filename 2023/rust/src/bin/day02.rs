f0e309d18b97622f4b4c9ef9bede249c12e55040
Stu Perry <stewart.c.perry@gmail.com>
2023-12-03 15:00:46 +0000

    Working day02 parts 1 & 2

--- 2023/rust/src/bin/day02.rs
+++ 2023/rust/src/bin/day02.rs
@@ -10,12 +10,36 @@ fn main() {
     let contents = fs::read_to_string(filename).expect("Couldn't read file {filename}");
     let contents: Vec<_> = contents.lines().collect();
     part1(&contents);
+    part2(&contents);
 }
 
-fn parse_lines<F1, F2>(lines: &[&str], color_tester: F1, mut totalizer: F2)
+fn parse_line_groups<F1>(groups: &str, color_tester: &mut F1) -> bool
 where
-    F1: Fn(&str, u32) -> bool,
-    F2: FnMut(bool, u32),
+    F1: FnMut(&str, u32) -> bool,
+{
+    for group in groups.split(';') {
+        for cube_count in group.split(',') {
+            let mut toks = cube_count
+                .strip_prefix(' ')
+                .expect("not whitespace padded")
+                .split(' ');
+            let count = toks
+                .next()
+                .expect("couldn't tokenize block count \"{color}\"")
+                .parse::<u32>()
+                .expect("unable to parse cube count in \"{group}\"");
+            let cube_color = toks.next().expect("missing color token in group");
+            if !color_tester(cube_color, count) {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+fn parse_lines<F1>(lines: &[&str], mut line_processor: F1)
+where
+    F1: FnMut(u32, &str),
 {
     for &line in lines {
         if let Some(remain) = line.strip_prefix("Game ") {
@@ -26,35 +50,15 @@ where
                 .collect::<String>()
                 .parse()
                 .expect("unable to parse game ID");
-            let _ = iter.by_ref().take_while(|c| !c.is_numeric());
-            let groups = iter.as_str();
-            let mut game_possible = true;
-            'outer: for group in groups.split(';') {
-                for cube_count in group.split(',') {
-                    let mut toks = cube_count
-                        .strip_prefix(' ')
-                        .expect("not whitespace padded")
-                        .split(' ');
-                    let count = toks
-                        .next()
-                        .expect("couldn't tokenize block count \"{color}\"")
-                        .parse::<u32>()
-                        .expect("unable to parse cube count in \"{group}\"");
-                    let cube_color = toks.next().expect("missing color token in group");
-                    if !color_tester(cube_color, count) {
-                        game_possible = false;
-                        break 'outer;
-                    }
-                }
-            }
-            totalizer(game_possible, game_id);
+            let _ = iter.by_ref().take_while(|c| !c.is_numeric()); // eat whitespace
+            line_processor(game_id, iter.as_str());
         }
     }
 }
 
 fn part1(lines: &[&str]) {
     let max_cubes = [("red", 12), ("green", 13), ("blue", 14)];
-    let test_cube_count = |cube_color: &str, cube_count| {
+    let mut test_cube_count = |cube_color: &str, cube_count| {
         for &(color, max_n) in max_cubes.iter() {
             if cube_color == color {
                 return cube_count <= max_n;
@@ -64,10 +68,50 @@ fn part1(lines: &[&str]) {
     };
 
     let mut total: u32 = 0;
-    parse_lines(lines, test_cube_count, |game_possible, game_id| {
-        if game_possible {
+    parse_lines(lines, |game_id, groups: &str| {
+        if parse_line_groups(groups, &mut test_cube_count) {
             total += game_id;
         }
     });
     println!("part1 total is {total}")
+}
+
+struct Cube {
+    pub color: &'static str,
+    pub max_count: u32,
+}
+
+impl Cube {
+    fn new(color: &'static str) -> Self {
+        Self {
+            color,
+            max_count: 0,
+        }
+    }
+    fn record_max(&mut self, count: u32) {
+        self.max_count = std::cmp::max(self.max_count, count);
+    }
+}
+
+fn part2(lines: &[&str]) {
+    let mut total: u32 = 0;
+    let line_parser = |_, groups: &str| {
+        let mut cube_colors = [Cube::new("red"), Cube::new("green"), Cube::new("blue")];
+        let mut cube_max_recorder = |cube_color: &str, cube_count: u32| {
+            for cube in cube_colors.iter_mut() {
+                if cube_color == cube.color {
+                    cube.record_max(cube_count);
+                    return true;
+                }
+            }
+            panic!("unexpected color \"{cube_color}\"");
+        };
+        parse_line_groups(groups, &mut cube_max_recorder);
+        let power = cube_colors
+            .iter()
+            .fold(1, |total, cube| cube.max_count * total);
+        total += power;
+    };
+    parse_lines(lines, line_parser);
+    println!("part2 total is {total}")
 }
