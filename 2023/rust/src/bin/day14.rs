1c213776bbb7af98ffb4eedf8be5ff2ad735645b
Stewart Perry <stewart.c.perry@gmail.com>
2023-12-15 10:51:53 +0000

    Day 14 part 2 work in progress

--- 2023/rust/src/bin/day14.rs
+++ 2023/rust/src/bin/day14.rs
@@ -19,12 +19,25 @@ fn main() {
 }
 
 fn part1(contents: &str) -> usize {
-    let mut columns = read_columns(contents);
-    columns.iter_mut().for_each(tilt_column);
+    let columns = read_columns(contents);
+    columns.iter().for_each(|c| tilt_column(c, true));
     columns.iter().map(get_score).sum()
 }
 
-fn part2(_contents: &str) -> usize {
+fn part2(contents: &str) -> usize {
+    let columns = read_columns(contents);
+    let cycle = || {
+        columns.iter().for_each(|c| tilt_column(c, true));
+        (0..columns[0].len()).for_each(|row_idx| tilt_row(&columns, row_idx, true));
+        columns.iter().for_each(|c| tilt_column(c, false));
+        (0..columns[0].len()).for_each(|row_idx| tilt_row(&columns, row_idx, false));
+        println!("");
+        print!("{}\n", cols_to_string(&columns));
+    };
+    for _ in 0..1000 {
+        cycle();
+    }
+    //columns.iter().map(get_score).sum()
     0
 }
 
@@ -43,17 +56,57 @@ fn get_score(col: &Column) -> usize {
         .sum()
 }
 
-fn tilt_column(col: &mut Column) {
+fn tilt_column(col: &Column, to_north: bool) {
+    let iter: Box<dyn Iterator<Item = &Cell<char>>> = if to_north {
+        Box::new(col.iter())
+    } else {
+        Box::new(col.iter().rev())
+    };
     let mut last_stop = 0;
-    for (idx, c) in col.iter().enumerate() {
+    for (idx, c) in iter.enumerate() {
         match c.get() {
             '#' => last_stop = idx + 1,
             'O' => {
                 if idx != last_stop {
-                    col[last_stop].replace('O');
+                    let last = if to_north {
+                        &col[last_stop]
+                    } else {
+                        &col[col.len() - last_stop - 1]
+                    };
+                    last.replace('O');
                     c.replace('.');
                 }
                 last_stop += 1;
+            }
+            _ => (),
+        }
+    }
+}
+
+fn tilt_row(cols: &[Column], row_idx: usize, to_west: bool) {
+    let mut last_stop = if to_west { 0 } else { cols.len() - 1 };
+    for i in 0..cols.len() {
+        let col_idx = if to_west { i } else { cols.len() - i - 1 };
+        let c = &cols[col_idx][row_idx];
+        match c.get() {
+            '#' => {
+                last_stop = if to_west {
+                    col_idx + 1
+                } else {
+                    col_idx.saturating_sub(1)
+                }
+            }
+            'O' => {
+                if col_idx != last_stop {
+                    let last = &cols[last_stop][row_idx];
+                    last.replace('O');
+                    c.replace('.');
+                }
+                last_stop = if to_west {
+                    last_stop + 1
+                } else {
+                    last_stop - 1
+                };
             }
             _ => (),
         }
@@ -78,8 +131,29 @@ fn read_columns(contents: &str) -> Vec<Column> {
     vlines
 }
 
+#[allow(dead_code)] // for testing
 fn to_string(col: &Column) -> String {
     String::from_iter(col.iter().map(|c| c.take()))
+}
+
+#[allow(dead_code)] // for testing
+fn cols_to_string(cols: &[Column]) -> String {
+    let mut rows: Vec<Vec<char>> = Vec::new();
+    for (idx, col) in cols.iter().enumerate() {
+        if 0 == idx {
+            for c in col.iter() {
+                rows.push(vec![c.get()]);
+            }
+        } else {
+            for (row_idx, c) in col.iter().enumerate() {
+                rows[row_idx].push(c.get());
+            }
+        }
+    }
+    rows.iter()
+        .map(|r| String::from_iter(r.iter()))
+        .collect::<Vec<_>>()
+        .join("\n")
 }
 
 #[cfg(test)]
@@ -102,8 +176,8 @@ mod test14 {
     #[test]
     fn GIVEN_column_WHEN_tilting_THEN_rocks_roll_up() {
         let dotest = |columns: &str, expected: &str| {
-            let mut col = columns.chars().map(|c| Cell::new(c)).collect::<Vec<_>>();
-            tilt_column(&mut col);
+            let col = columns.chars().map(Cell::new).collect::<Vec<_>>();
+            tilt_column(&col, true);
             assert_eq!(expected, to_string(&col).as_str());
         };
         dotest(".O", "O.");
@@ -113,9 +187,59 @@ mod test14 {
     }
 
     #[test]
+    fn GIVEN_column_WHEN_tilting_south_THEN_rocks_roll_down() {
+        let dotest = |column: &str, expected: &str| {
+            let col = column.chars().map(Cell::new).collect::<Vec<_>>();
+            tilt_column(&col, false);
+            assert_eq!(expected, to_string(&col).as_str());
+        };
+        dotest(".O", ".O");
+        dotest(".#.O#....O", ".#.O#....O");
+        dotest(".#O.#...O.", ".#.O#....O");
+        dotest("OO.O.O..##", "....OOOO##");
+    }
+    #[test]
+    fn GIVEN_columns_WHEN_tilting_west_THEN_rocks_roll_left() {
+        let dotest = |columns: &str, expected: &str| {
+            let cols = columns
+                .chars()
+                .map(|c| vec![Cell::new(c)])
+                .collect::<Vec<_>>();
+            tilt_row(&cols, 0, true);
+            assert_eq!(
+                expected,
+                to_string(&cols.iter().map(|c| c[0].clone()).collect::<Vec<_>>()).as_str()
+            );
+        };
+        dotest(".O", "O.");
+        dotest(".#.O#....O", ".#O.#O....");
+        dotest(".#.O#....O", ".#O.#O....");
+        dotest("OO.O.O..##", "OOOO....##");
+    }
+    #[test]
+    fn GIVEN_columns_WHEN_tilting_east_THEN_rocks_roll_right() {
+        let dotest = |columns: &str, expected: &str| {
+            let cols = columns
+                .chars()
+                .map(|c| vec![Cell::new(c)])
+                .collect::<Vec<_>>();
+            tilt_row(&cols, 0, false);
+            assert_eq!(
+                expected,
+                to_string(&cols.iter().map(|c| c[0].clone()).collect::<Vec<_>>()).as_str()
+            );
+        };
+        dotest("O.", ".O");
+        dotest("#O.", "#.O"); // tests for underflow
+        dotest(".#.O#..O.O", ".#.O#...OO");
+        dotest(".#O.#...O.", ".#.O#....O");
+        dotest("OO.O.O..##", "....OOOO##");
+    }
+
+    #[test]
     fn GIVEN_column_WHEN_scoring_THEN_correct_score_returned() {
         let dotest = |columns: &str, expected: usize| {
-            let col = columns.chars().map(|c| Cell::new(c)).collect::<Vec<_>>();
+            let col = columns.chars().map(Cell::new).collect::<Vec<_>>();
             assert_eq!(get_score(&col), expected);
         };
         dotest("#", 0);
@@ -136,7 +260,12 @@ O.#..O.#.#
 #OO..#...."#;
 
     #[test]
-    fn GIVEN_aoc_example_WHEN_part2_run_THEN_matches_expected() {
+    fn GIVEN_aoc_example_WHEN_part1_run_THEN_matches_expected() {
         assert_eq!(136, part1(EXAMPLE));
+    }
+
+    #[test]
+    fn GIVEN_aoc_example_WHEN_part2_run_THEN_matches_expected() {
+        assert_eq!(64, part2(EXAMPLE));
     }
 }
