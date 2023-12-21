49f8e971805b9f44816c156e7ca6ed8fcff80569
Stu Perry <stewart.c.perry@gmail.com>
2023-12-21 10:05:56 +0000

    Working day 18 part 2

--- 2023/rust/src/bin/day18.rs
+++ 2023/rust/src/bin/day18.rs
@@ -19,6 +19,7 @@ fn main() {
 }
 
 fn part1(contents: &str) -> usize {
+    // Solved using a bitmap and flood-fill algorithm, similar to Day 10.
     use FillSegment::*;
     let trenches = contents
         .lines()
@@ -56,6 +57,73 @@ fn part1(contents: &str) -> usize {
         .sum()
 }
 
+fn part2(contents: &str) -> usize {
+    // The bitmap approach cannot be used here due to the very large numbers
+    // involved. Instead this is solved using a polygon area formula - the main
+    // difficulty with this is accounting for the width of the trench (see
+    // below).
+    let trenches = contents
+        .lines()
+        .filter(|l| !l.trim().is_empty())
+        .map(Trench::new_from_color)
+        .collect::<Vec<_>>();
+
+    let vertices = get_outside_vertices(&trenches);
+    area_from_shoelace_formula(&vertices) as usize
+}
+
+fn get_outside_vertices(trenches: &[Trench]) -> Vec<(i64, i64)> {
+    // the perimiter of the trench is one unit larger for outside corners and
+    // one unit smaller for inside corners. The following assumes clockwise
+    // rotation.
+    let mut vertices = Vec::<(i64, i64)>::with_capacity(trenches.len() + 1);
+    use Direction::*;
+    let mut point = (0, 0);
+    for (idx, trench) in trenches.iter().enumerate() {
+        let prev = &trenches[if idx == 0 { trenches.len() - 1 } else { idx - 1 }];
+        let next = &trenches[if idx == trenches.len() - 1 { 0 } else { idx + 1 }];
+        let len = trench.length as i64;
+        let vec = match trench.direction {
+            Up => {
+                let offset = match (prev.direction, next.direction) {
+                    (Left, Right) => 1,
+                    (Right, Left) => -1,
+                    _ => 0,
+                };
+                (0, len + offset)
+            }
+            Down => {
+                let offset = match (prev.direction, next.direction) {
+                    (Right, Left) => 1,
+                    (Left, Right) => -1,
+                    _ => 0,
+                };
+                (0, -len - offset)
+            }
+            Left => {
+                let offset = match (prev.direction, next.direction) {
+                    (Down, Up) => 1,
+                    (Up, Down) => -1,
+                    _ => 0,
+                };
+                (-len - offset, 0)
+            }
+            Right => {
+                let offset = match (prev.direction, next.direction) {
+                    (Up, Down) => 1,
+                    (Down, Up) => -1,
+                    _ => 0,
+                };
+                (len + offset, 0)
+            }
+        };
+        point.0 += vec.0;
+        point.1 += vec.1;
+        vertices.push(point);
+    }
+    vertices
+}
+
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 #[repr(u8)]
 enum FillSegment {
@@ -77,8 +145,8 @@ fn flood_fill(start_point: (usize, usize), direction: Direction, rows: &mut [Vec
         Right => ((0, 1), (0, -1)),
     };
     let mut point = start_point;
-    point.0 = (point.0 as i32 + lhs.0) as usize;
-    point.1 = (point.1 as i32 + lhs.1) as usize;
+    point.0 = (point.0 as i64 + lhs.0) as usize;
+    point.1 = (point.1 as i64 + lhs.1) as usize;
     while dy.contains(&(point.1)) && dx.contains(&(point.0)) {
         let slot = &mut rows[point.1][point.0];
         match *slot {
@@ -87,12 +155,12 @@ fn flood_fill(start_point: (usize, usize), direction: Direction, rows: &mut [Vec
             LeftSide => (),
             RightSide => panic!("inconsistent fill segments"),
         }
-        point.0 = (point.0 as i32 + lhs.0) as usize;
-        point.1 = (point.1 as i32 + lhs.1) as usize;
+        point.0 = (point.0 as i64 + lhs.0) as usize;
+        point.1 = (point.1 as i64 + lhs.1) as usize;
     }
     let mut point = start_point;
-    point.0 = (point.0 as i32 + rhs.0) as usize;
-    point.1 = (point.1 as i32 + rhs.1) as usize;
+    point.0 = (point.0 as i64 + rhs.0) as usize;
+    point.1 = (point.1 as i64 + rhs.1) as usize;
     while dy.contains(&(point.1)) && dx.contains(&(point.0)) {
         let slot = &mut rows[point.1][point.0];
         match *slot {
@@ -101,8 +169,8 @@ fn flood_fill(start_point: (usize, usize), direction: Direction, rows: &mut [Vec
             RightSide => (),
             LeftSide => panic!("inconsistent fill segments"),
         }
-        point.0 = (point.0 as i32 + rhs.0) as usize;
-        point.1 = (point.1 as i32 + rhs.1) as usize;
+        point.0 = (point.0 as i64 + rhs.0) as usize;
+        point.1 = (point.1 as i64 + rhs.1) as usize;
     }
 }
 
@@ -121,27 +189,27 @@ where
         };
         callback(point, trench.direction);
         for _ in 0..trench.length {
-            point.0 = (point.0 as i32 + vec.0) as usize;
-            point.1 = (point.1 as i32 + vec.1) as usize;
+            point.0 = (point.0 as i64 + vec.0) as usize;
+            point.1 = (point.1 as i64 + vec.1) as usize;
             callback(point, trench.direction);
         }
     }
 }
 
-fn get_bounds(trenches: &[Trench]) -> (RangeInclusive<i32>, RangeInclusive<i32>) {
+fn get_bounds(trenches: &[Trench]) -> (RangeInclusive<i64>, RangeInclusive<i64>) {
     use Direction::*;
-    let mut x: i32 = 0;
-    let mut y: i32 = 0;
-    let mut min_x: i32 = i32::MAX;
-    let mut min_y: i32 = i32::MAX;
-    let mut max_x: i32 = i32::MIN;
-    let mut max_y: i32 = i32::MIN;
+    let mut x: i64 = 0;
+    let mut y: i64 = 0;
+    let mut min_x: i64 = i64::MAX;
+    let mut min_y: i64 = i64::MAX;
+    let mut max_x: i64 = i64::MIN;
+    let mut max_y: i64 = i64::MIN;
     for trench in trenches.iter() {
         match trench.direction {
-            Up => y += trench.length as i32,
-            Down => y -= trench.length as i32,
-            Left => x -= trench.length as i32,
-            Right => x += trench.length as i32,
+            Up => y += trench.length as i64,
+            Down => y -= trench.length as i64,
+            Left => x -= trench.length as i64,
+            Right => x += trench.length as i64,
         }
         min_x = x.min(min_x);
         min_y = y.min(min_y);
@@ -149,10 +217,6 @@ fn get_bounds(trenches: &[Trench]) -> (RangeInclusive<i32>, RangeInclusive<i32>)
         max_y = y.max(max_y);
     }
     (min_x..=max_x, min_y..=max_y)
-}
-
-fn part2(_contents: &str) -> usize {
-    0
 }
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
@@ -179,7 +243,6 @@ impl Direction {
 struct Trench {
     direction: Direction,
     length: usize,
-    color: [u8; 3],
 }
 
 impl Trench {
@@ -194,27 +257,45 @@ impl Trench {
             .unwrap_or_else(|| panic!("no length in {line}"))
             .parse::<usize>()
             .expect("non-numeric length");
-        let color = parse_color(toks.next().unwrap_or_else(|| panic!("no color in {line}")));
-        Self {
-            direction,
-            length,
-            color,
-        }
+        Self { direction, length }
+    }
+
+    fn new_from_color(line: &str) -> Self {
+        let parse_color = |rgb: &str| {
+            let rgb = rgb
+                .chars()
+                .filter(|c| c.is_ascii_hexdigit())
+                .collect::<String>();
+            assert_eq!(6, rgb.len());
+            let len = usize::from_str_radix(&rgb[0..5], 16).unwrap();
+            use Direction::*;
+            let dir = match &rgb[5..6] {
+                "0" => Right,
+                "1" => Down,
+                "2" => Left,
+                "3" => Up,
+                _ => panic!("unknown direction {rgb}"),
+            };
+            (len, dir as Direction)
+        };
+        let mut toks = line.split(' ').skip(2);
+        let (length, direction) =
+            parse_color(toks.next().unwrap_or_else(|| panic!("no color in {line}")));
+        Self { direction, length }
     }
 }
 
-fn parse_color(rgb: &str) -> [u8; 3] {
-    let rgb = rgb
-        .chars()
-        .filter(|c| c.is_ascii_hexdigit())
-        .collect::<String>();
-    assert_eq!(6, rgb.len());
-    let mut result = [0, 0, 0];
-    for (idx, color) in result.iter_mut().enumerate() {
-        let start = idx * 2;
-        *color = u8::from_str_radix(&rgb[start..2 + start], 16).unwrap();
-    }
-    result
+fn area_from_shoelace_formula(vertices: &[(i64, i64)]) -> i64 {
+    // https://en.wikipedia.org/wiki/Shoelace_formula
+    let total: i64 = vertices
+        .iter()
+        .enumerate()
+        .map(|(idx, xy)| {
+            let xyprime = vertices[if idx == 0 { vertices.len() - 1 } else { idx - 1 }];
+            xy.0 * xyprime.1 - xy.1 * xyprime.0
+        })
+        .sum();
+    total / 2
 }
 
 #[cfg(test)]
@@ -224,23 +305,31 @@ mod test18 {
 
     #[test]
     fn GIVEN_valid_line_WHEN_constructing_trench_THEN_correct_struct_returned() {
-        let dotest = |line, dir, len, rgb| {
+        let dotest = |line, dir, len| {
             let trench = Trench::new(line);
             assert_eq!(dir, trench.direction);
             assert_eq!(len, trench.length);
-            assert_eq!(rgb, trench.color);
         };
-        use Direction::*;
-        dotest(r"U 4 (#640bb1)", Up, 4, [0x64, 0xb, 0xb1]);
+        dotest(r"U 4 (#640bb1)", Direction::Up, 4);
+    }
+
+    #[test]
+    fn GIVEN_valid_line_WHEN_constructing_trench_from_color_THEN_correct_struct_returned() {
+        let dotest = |line, len, dir| {
+            let trench = Trench::new_from_color(line);
+            assert_eq!(dir, trench.direction);
+            assert_eq!(len, trench.length);
+        };
+        dotest(r"U 4 (#640bb1)", 0x640bb, Direction::Down);
     }
 
     #[test]
     fn GIVEN_small_grid_WHEN_flood_filling_THEN_expected_slots_filled() {
         let grid = r#"
-U 3 (#FFFFFF) 
-R 3 (#FFFFFF) 
-D 3 (#FFFFFF) 
-L 3 (#FFFFFF) "#;
+U 3 (#FFFFF0) 
+R 3 (#FFFFF0) 
+D 3 (#FFFFF0) 
+L 3 (#FFFFF0) "#;
         let mut rows = Vec::<Vec<FillSegment>>::new();
         for _ in 0..6 {
             rows.push(vec![FillSegment::Unknown; 6]);
@@ -276,6 +365,14 @@ L 3 (#FFFFFF) "#;
         }
     }
 
+    #[test]
+    fn GIVEN_example_polygon_WHEN_calcing_area_THEN_correct_answer_returned() {
+        let square = [(0, 0), (0, 3), (3, 3), (3, 0)];
+        assert_eq!(9, area_from_shoelace_formula(&square));
+        let square = [(2, 2), (2, -2), (-2, -2), (-2, 2)];
+        assert_eq!(16, area_from_shoelace_formula(&square));
+    }
+
     static EXAMPLE: &str = r#"
 R 6 (#70c710)
 D 5 (#0dc571)
@@ -295,5 +392,21 @@ U 2 (#7a21e3)"#;
     #[test]
     fn GIVEN_aoc_example_WHEN_part1_run_THEN_matches_expected() {
         assert_eq!(62, part1(EXAMPLE));
+    }
+
+    #[test]
+    fn GIVEN_aoc_example_WHEN_calcing_part1_area_THEN_matches_part1_answer() {
+        let trenches = EXAMPLE
+            .lines()
+            .filter(|l| !l.trim().is_empty())
+            .map(Trench::new)
+            .collect::<Vec<_>>();
+        let vertices = get_outside_vertices(&trenches);
+        assert_eq!(62, area_from_shoelace_formula(&vertices));
+    }
+
+    #[test]
+    fn GIVEN_aoc_example_WHEN_part2_run_THEN_matches_expected() {
+        assert_eq!(952408144115, part2(EXAMPLE));
     }
 }
