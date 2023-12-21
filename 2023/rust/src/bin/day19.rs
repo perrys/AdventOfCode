93aab17ce88c1863e726da8226b4933b14bb7822
Stu Perry <stewart.c.perry@gmail.com>
2023-12-21 20:59:58 +0000

    Day 19 working part 2

--- 2023/rust/src/bin/day19.rs
+++ 2023/rust/src/bin/day19.rs
@@ -3,7 +3,7 @@
 //!
 //! See <https://adventofcode.com/2023/day/19>
 //!
-use std::{collections::HashMap, fs};
+use std::{collections::HashMap, fs, ops::Range};
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -18,29 +18,48 @@ fn main() {
     println!("part2 total is {}", part2(contents.as_str()));
 }
 
-fn part1(contents: &str) -> i32 {
+fn part1(contents: &str) -> i64 {
     let workflows = contents
         .lines()
-        .filter(|l| !l.trim().is_empty() && !(l.chars().nth(0).unwrap() == '{'))
+        .filter(|l| !l.trim().is_empty() && !l.starts_with('{'))
         .map(Workflow::new)
         .collect::<Vec<_>>();
     let workflows = HashMap::from_iter(workflows.into_iter().map(|wf| (wf.id.clone(), wf)));
     let parts = contents
         .lines()
-        .filter(|l| !l.trim().is_empty() && (l.chars().nth(0).unwrap() == '{'))
+        .filter(|l| !l.trim().is_empty() && l.starts_with('{'))
         .map(Part::new)
         .collect::<Vec<_>>();
     parts.iter().map(|part| process(part, &workflows)).sum()
 }
 
-fn part2(_contents: &str) -> usize {
-    0
+fn part2(contents: &str) -> i64 {
+    let workflows = contents
+        .lines()
+        .filter(|l| !l.trim().is_empty() && !l.starts_with('{'))
+        .map(Workflow::new)
+        .collect::<Vec<_>>();
+    let workflows = HashMap::from_iter(workflows.into_iter().map(|wf| (wf.id.clone(), wf)));
+    let rule_list = consolidate(&workflows);
+    rule_list
+        .iter()
+        .map(|predicates| -> i64 {
+            let result = "xmas"
+                .chars()
+                .map(|attribute| {
+                    let range = merge_attribute_predicates(predicates, attribute, 1..4001);
+                    range.end - range.start
+                })
+                .product();
+            result
+        })
+        .sum()
 }
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
 struct Predicate {
     attribute: char,
-    value: i32,
+    value: i64,
     is_less_than: bool,
 }
 
@@ -64,7 +83,7 @@ struct Workflow {
 }
 
 #[derive(Debug, Eq, PartialEq)]
-struct Part(HashMap<char, i32>);
+struct Part(HashMap<char, i64>);
 
 impl Part {
     fn new(line: &str) -> Self {
@@ -83,7 +102,7 @@ impl Part {
             let val = kv
                 .next()
                 .unwrap_or_else(|| panic!("couldn't get value in {s}"))
-                .parse::<i32>()
+                .parse::<i64>()
                 .unwrap_or_else(|_| panic!("couldn't parse value in {s}"));
             attrs.insert(key.to_owned(), val);
         });
@@ -110,7 +129,7 @@ impl Rule {
                 _ => panic!("unrecognized operator in predicate {pred_str}"),
             };
             let value = pred_str[2..]
-                .parse::<i32>()
+                .parse::<i64>()
                 .unwrap_or_else(|_| panic!("couldn't parse number in predicate {pred_str}"));
             predicate = Some(Predicate {
                 attribute,
@@ -124,6 +143,33 @@ impl Rule {
             _ => Action::ToWorkflow(action.to_owned()),
         };
         Self { predicate, action }
+    }
+
+    fn passes(&self, part: &Part) -> bool {
+        if let Some(p_fn) = &self.predicate {
+            let tesval = part.0[&p_fn.attribute];
+            match p_fn.is_less_than {
+                true => tesval < p_fn.value,
+                false => tesval > p_fn.value,
+            }
+        } else {
+            true
+        }
+    }
+}
+
+impl Predicate {
+    fn opposite(&self) -> Self {
+        let attribute = self.attribute;
+        let value = match self.is_less_than {
+            true => self.value - 1,
+            false => self.value + 1,
+        };
+        Self {
+            attribute,
+            is_less_than: !self.is_less_than,
+            value,
+        }
     }
 }
 
@@ -141,17 +187,11 @@ impl Workflow {
     }
 }
 
-fn process(part: &Part, workflows: &HashMap<String, Workflow>) -> i32 {
+fn process(part: &Part, workflows: &HashMap<String, Workflow>) -> i64 {
     let mut workflow = workflows.get("in").expect("starting workflow not found");
     'l1: loop {
         for rule in workflow.rules.iter() {
-            let mut p_result = true;
-            if let Some(p_fn) = &rule.predicate {
-                let tesval = part.0[&p_fn.attribute];
-                p_result =
-                    if p_fn.is_less_than { tesval < p_fn.value } else { tesval > p_fn.value };
-            }
-            if p_result {
+            if rule.passes(part) {
                 match &rule.action {
                     Action::ToWorkflow(id) => {
                         workflow = workflows
@@ -165,6 +205,66 @@ fn process(part: &Part, workflows: &HashMap<String, Workflow>) -> i32 {
             }
         }
     }
+}
+
+fn consolidate(workflows: &HashMap<String, Workflow>) -> Vec<Vec<Predicate>> {
+    fn recurse(
+        workflows: &HashMap<String, Workflow>,
+        workflow: &Workflow,
+        mut predicates: Vec<Predicate>,
+    ) -> Vec<Vec<Predicate>> {
+        let mut result = Vec::new();
+        for rule in workflow.rules.iter() {
+            match &rule.action {
+                Action::ToWorkflow(id) => {
+                    if let Some(p_fn) = rule.predicate {
+                        let mut newlist = predicates.clone();
+                        newlist.push(p_fn);
+                        let mut sub_rules = recurse(workflows, &workflows[id], newlist);
+                        result.append(&mut sub_rules);
+                        predicates.push(p_fn.opposite());
+                    } else {
+                        let mut sub_rules = recurse(workflows, &workflows[id], predicates.clone());
+                        result.append(&mut sub_rules);
+                    }
+                }
+                Action::Accept => {
+                    if let Some(p_fn) = rule.predicate {
+                        let mut newlist = predicates.clone();
+                        newlist.push(p_fn);
+                        result.push(newlist);
+                        predicates.push(p_fn.opposite());
+                    } else {
+                        result.push(predicates.clone());
+                    }
+                }
+                Action::Reject => {
+                    if let Some(p_fn) = rule.predicate {
+                        predicates.push(p_fn.opposite());
+                    }
+                }
+            }
+        }
+        result
+    }
+    let workflow = workflows.get("in").expect("starting workflow not found");
+    recurse(workflows, workflow, Vec::new())
+}
+
+fn merge_attribute_predicates(
+    predicates: &[Predicate],
+    attribute: char,
+    mut range: Range<i64>,
+) -> Range<i64> {
+    for p in predicates.iter() {
+        if p.attribute == attribute {
+            match p.is_less_than {
+                true => range.end = range.end.min(p.value),
+                false => range.start = range.start.max(p.value + 1),
+            }
+        }
+    }
+    range
 }
 
 #[cfg(test)]
@@ -257,6 +357,77 @@ mod test19 {
         assert_eq!(19114, part1(&EXAMPLE[1..]));
     }
 
+    #[test]
+    fn GIVEN_simple_workflows_WHEN_consolidating_THEN_accepteance_predicates_returned() {
+        let workflows = [
+            Workflow::new("in{s<1000:n1,a<1000:R,A}"),
+            Workflow::new("n1{x>1000:R,m>1000:A,R}"),
+        ];
+        let workflows = HashMap::from_iter(workflows.into_iter().map(|wf| (wf.id.clone(), wf)));
+        let rule_list = consolidate(&workflows);
+        println! {"{rule_list:?}"};
+        assert_eq!(
+            rule_list,
+            vec![
+                vec![
+                    Predicate {
+                        attribute: 's',
+                        value: 1000,
+                        is_less_than: true
+                    },
+                    Predicate {
+                        attribute: 'x',
+                        value: 1001,
+                        is_less_than: true
+                    },
+                    Predicate {
+                        attribute: 'm',
+                        value: 1000,
+                        is_less_than: false
+                    }
+                ],
+                vec![
+                    Predicate {
+                        attribute: 's',
+                        value: 999,
+                        is_less_than: false
+                    },
+                    Predicate {
+                        attribute: 'a',
+                        value: 999,
+                        is_less_than: false
+                    }
+                ]
+            ]
+        );
+    }
+
+    #[test]
+    fn GIVEN_predicate_list_WHEN_merging_THEN_merged_range_returned() {
+        let p_list = vec![
+            Predicate {
+                attribute: 's',
+                value: 1000,
+                is_less_than: true,
+            },
+            Predicate {
+                attribute: 's',
+                value: 1500,
+                is_less_than: true,
+            },
+            Predicate {
+                attribute: 's',
+                value: 900,
+                is_less_than: false,
+            },
+        ];
+        assert_eq!(901..1000, merge_attribute_predicates(&p_list, 's', 0..4001));
+    }
+
+    #[test]
+    fn GIVEN_aoc_example_WHEN_part2_run_THEN_matches_expected() {
+        assert_eq!(167409079868000, part2(&EXAMPLE[1..]));
+    }
     static EXAMPLE: &str = r#"
 px{a<2006:qkq,m>2090:A,rfg}
 pv{a>1716:R,A}
