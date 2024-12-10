4db4664d5f4417c6a9d67eb8e86effb496f708d4
Stu Perry <stewart.c.perry@gmail.com>
2024-12-10 20:38:04 +0000

    Working day09 both parts

--- 2024/cpp/src/bin/day09.cpp
+++ 2024/cpp/src/bin/day09.cpp
@@ -20,14 +20,15 @@
  */
 
 namespace {
-struct FileBlock {
+
+struct BlockGroup {
     size_t start_pos;
     size_t length;
     std::optional<int> id;
 };
 
-void parse(const std::string& line, std::vector<FileBlock>& filelist,
-           std::vector<FileBlock>& freelist) {
+void parse(const std::string& line, std::vector<BlockGroup>& filelist,
+           std::vector<BlockGroup>& freelist) {
     size_t startPos = 0;
     size_t fileId = 0;
     for (size_t i = 0; i < line.length(); ++i) {
@@ -43,6 +44,80 @@ void parse(const std::string& line, std::vector<FileBlock>& filelist,
         startPos += fileSize;
     }
 }
+std::vector<BlockGroup> compressFragment(std::vector<BlockGroup>& filelist,
+                                         std::vector<BlockGroup>& freelist) {
+    std::vector<BlockGroup> newFiles;
+    BlockGroup movedFile;
+    std::reverse(freelist.begin(), freelist.end());
+
+    while (true) {
+        if (freelist.empty() || filelist.empty()) {
+            break;
+        }
+        BlockGroup& endFile = (*filelist.rbegin());
+        BlockGroup& firstFree = (*freelist.rbegin());
+        if (endFile.start_pos < firstFree.start_pos) {
+            break;
+        }
+        size_t nblocks = std::min(endFile.length, firstFree.length);
+        if (!firstFree.id) {
+            firstFree.id = endFile.id.value();
+        }
+        endFile.length -= nblocks;
+        firstFree.length -= nblocks;
+        if (0 == endFile.length) {
+            filelist.pop_back();
+            if (firstFree.length > 0) { // split the free block
+                newFiles.emplace_back(firstFree.start_pos, nblocks, firstFree.id);
+                firstFree.start_pos += nblocks;
+                firstFree.id = {};
+            }
+        }
+        if (0 == firstFree.length) {
+            newFiles.emplace_back(firstFree.start_pos, nblocks, firstFree.id);
+            freelist.pop_back();
+        }
+    }
+    return newFiles;
+}
+
+std::vector<BlockGroup> compressNonFragment(std::vector<BlockGroup>& filelist,
+                                            std::vector<BlockGroup>& freelist) {
+    std::vector<BlockGroup> newFiles;
+    BlockGroup movedFile;
+
+    for (auto& lastFile : std::ranges::reverse_view(filelist)) {
+        if (freelist.empty()) {
+            break;
+        }
+        for (auto& freeblock : freelist) {
+            if (freeblock.length >= lastFile.length && freeblock.start_pos < lastFile.start_pos) {
+                newFiles.emplace_back(freeblock.start_pos, lastFile.length, lastFile.id.value());
+                freeblock.length -= lastFile.length;
+                freeblock.start_pos += lastFile.length;
+                lastFile.length = 0;
+                break;
+            }
+        }
+    }
+    return newFiles;
+}
+
+size_t checksum(const std::vector<BlockGroup>& filelist, const std::vector<BlockGroup>& newFiles) {
+    size_t total = 0;
+    for (const auto& block : filelist) {
+        for (size_t i = 0; i < block.length; ++i) {
+            total += (block.start_pos + i) * block.id.value();
+        }
+    }
+    for (const auto& block : newFiles) {
+        for (size_t i = 0; i < block.length; ++i) {
+            total += (block.start_pos + i) * block.id.value();
+        }
+    }
+    return total;
+}
+
 } // namespace
 
 int main(int argc, char* argv[]) {
@@ -54,52 +129,18 @@ int main(int argc, char* argv[]) {
     }
 
     const auto contents = scp::getContents(arguments[1]);
-    std::vector<FileBlock> filelist;
-    std::vector<FileBlock> freelist;
-    parse(contents, filelist, freelist);
-    std::reverse(freelist.begin(), freelist.end());
-    std::vector<FileBlock> newFiles;
-    FileBlock movedFile;
 
-    while (true) {
-        if (freelist.empty() || filelist.empty()) {
-            break;
-        }
-        FileBlock& lastFile = (*filelist.rbegin());
-        FileBlock& nextFree = (*freelist.rbegin());
-        if (lastFile.start_pos < nextFree.start_pos) {
-            break;
-        }
-        size_t nblocks = std::min(lastFile.length, nextFree.length);
-        if (!nextFree.id) {
-            nextFree.id = lastFile.id.value();
-        }
-        lastFile.length -= nblocks;
-        nextFree.length -= nblocks;
-        if (0 == lastFile.length) {
-            filelist.pop_back();
-            if (nextFree.length > 0) { // split the free block
-                newFiles.emplace_back(nextFree.start_pos, nblocks, nextFree.id);
-                nextFree.start_pos += nblocks;
-                nextFree.id = {};
-            }
-        }
-        if (0 == nextFree.length) {
-            newFiles.emplace_back(nextFree.start_pos, nblocks, nextFree.id);
-            freelist.pop_back();
-        }
-    }
-    size_t part1Total = 0;
-    for (const auto& block : filelist) {
-        for (size_t i = 0; i < block.length; ++i) {
-            part1Total += (block.start_pos + i) * block.id.value();
-        }
-    }
-    for (const auto& block : newFiles) {
-        for (size_t i = 0; i < block.length; ++i) {
-            part1Total += (block.start_pos + i) * block.id.value();
-            // std::cout << "(" << block.id.value() << ", " << block.start_pos + i << ")" << std::endl;
-        }
-    }
-    std::cout << "part1 total = " << part1Total << std::endl;
+    std::vector<BlockGroup> filelist;
+    std::vector<BlockGroup> freelist;
+    parse(contents, filelist, freelist);
+
+    std::vector<BlockGroup> newFiles = compressFragment(filelist, freelist);
+    std::cout << "part1 total = " << checksum(filelist, newFiles) << std::endl;
+
+    freelist.clear();
+    filelist.clear();
+    parse(contents, filelist, freelist);
+
+    newFiles = compressNonFragment(filelist, freelist);
+    std::cout << "part2 total = " << checksum(filelist, newFiles) << std::endl;
 }
