c7f6f7e8ff0be665713966773f8912094cf02d2d
Stu Perry <stewart.c.perry@gmail.com>
2024-12-23 00:13:02 +0000

    Day 19 part 2 solved (nice and quickly :)

--- 2024/cpp/src/bin/day19.cpp
+++ 2024/cpp/src/bin/day19.cpp
@@ -23,23 +23,27 @@
 
 namespace {
 
-std::optional<std::string> canMatch(std::string_view pattern,
-                                    const std::unordered_set<std::string_view>& stripes) {
+using Cache = std::unordered_map<std::string_view, size_t>;
+
+size_t canMatch(std::string_view pattern, Cache& cache,
+                const std::unordered_set<std::string_view>& stripes) {
     // std::cout << "\npattern: " << pattern << std::endl;
     if (pattern.empty()) {
-        return "";
+        return 1;
     }
+    if (cache.contains(pattern)) {
+        return cache.at(pattern);
+    }
+    size_t total = 0;
     for (size_t i = 1; i <= pattern.size(); ++i) {
         std::string_view sv(pattern.begin(), pattern.begin() + i);
         if (stripes.contains(sv)) {
             // std::cout << "matched \"" << sv << "\"" << std::endl;
-            auto lower = canMatch({pattern.begin() + i, pattern.end()}, stripes);
-            if (lower.has_value()) {
-                return std::string(sv) + lower.value();
-            }
+            total += canMatch({pattern.begin() + i, pattern.end()}, cache, stripes);
         }
     }
-    return {};
+    cache[pattern] = total;
+    return total;
 }
 
 } // namespace
@@ -64,13 +68,16 @@ int main(int argc, char* argv[]) {
         }
         start = submatch.second;
     }
-    size_t count = 0;
+    size_t part1Count = 0;
+    size_t part2Total = 0;
+    Cache cache;
     for (size_t i = 2; i < lines.size(); ++i) {
-        auto result = canMatch(lines[i], stripes);
-        if (result) {
-            assert(result == lines[i]);
-            count += 1;
+        size_t total = canMatch(lines[i], cache, stripes);
+        if (total > 0) {
+            part1Count += 1;
         }
+        part2Total += total;
     }
-    std::cout << "part 1 answer: " << count << std::endl;
+    std::cout << "part 1 answer: " << part1Count << std::endl;
+    std::cout << "part 2 answer: " << part2Total << std::endl;
 }
