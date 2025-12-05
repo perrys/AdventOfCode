661cf6e1875ebb3ea5f65f04c2eb9eba0888d478
Stu Perry <stewart.c.perry@gmail.com>
2025-12-05 07:56:05 +0000

    day3 complete

--- 2025/rust/src/bin/day02.rs
+++ 2025/rust/src/bin/day02.rs
@@ -1,13 +1,14 @@
+use itertools::Itertools;
 use std::{env, fs};
 
-struct Range {
+struct NumRange {
     lo: u64,
     hi: u64,
     lo_digits: Vec<char>,
     hi_digits: Vec<char>,
 }
 
-impl Range {
+impl NumRange {
     fn parse(tok: &str) -> Self {
         let mut toks = tok.split('-');
         let first = toks.next().expect("first element missing");
@@ -38,12 +39,28 @@ impl Range {
     }
 }
 
-fn process_digits(ndigits: usize, prefix: &[char], result: &mut Vec<Vec<char>>) {
-    if 1 == (ndigits & 1) {
-        return;
+fn factors(n: usize) -> Vec<usize> {
+    let mut result = vec![n];
+    for i in 2..n {
+        if i * i > n {
+            break;
+        }
+        if n.is_multiple_of(i) {
+            result.push(i);
+            if i != 1 && i * i != n {
+                result.push(n / i);
+            }
+        }
     }
-    let half = ndigits / 2;
-    if prefix.len() < half {
+    result.sort();
+    result
+}
+
+fn process_digits(ndigits: usize, divisor: usize, prefix: &[char], result: &mut Vec<Vec<char>>) {
+    //println!("nd: {}, div: {}", ndigits, divisor);
+    assert!(ndigits.is_multiple_of(divisor));
+    let pattern_length = ndigits / divisor;
+    if prefix.len() < pattern_length {
         let range = if prefix.is_empty() {
             '1'..='9'
         } else {
@@ -54,37 +71,43 @@ fn process_digits(ndigits: usize, prefix: &[char], result: &mut Vec<Vec<char>>) 
         new_prefix.push('0');
         for i in range {
             *new_prefix.last_mut().unwrap() = i;
-            process_digits(ndigits, &new_prefix, result);
+            process_digits(ndigits, divisor, &new_prefix, result);
         }
-    } else if prefix.len() == half {
+    } else if prefix.len() == pattern_length {
         let mut v = Vec::<_>::with_capacity(ndigits);
-        v.extend_from_slice(prefix);
-        v.extend_from_slice(prefix);
+        for _i in 0..divisor {
+            v.extend_from_slice(prefix);
+        }
         result.push(v);
     } else {
-        // prefix is more than half
+        // prefix is more than pattern_length
         let mut v = Vec::<_>::with_capacity(ndigits);
         v.extend_from_slice(prefix);
-        for i in 0..half {
-            let idx = i + half;
-            if idx < prefix.len() {
-                if prefix[idx] != prefix[i] {
-                    return;
+        v.resize(ndigits, ' ');
+        for i in 0..pattern_length {
+            for j in 1..divisor {
+                let idx = i + pattern_length * j;
+                if idx < prefix.len() {
+                    if prefix[idx] != prefix[i] {
+                        return;
+                    }
+                } else {
+                    v[idx] = v[i];
                 }
-            } else {
-                v.push(v[i]);
             }
         }
         result.push(v);
     }
 }
 
-fn process_range(range: &Range) -> Vec<u64> {
+fn p1_process(range: &NumRange) -> Vec<u64> {
     let common_prefix: Vec<char> = range.collect_prefix();
     let mut candidates = Vec::<_>::new();
     let mut ndigits = range.lo_digits.len();
     while ndigits <= range.hi_digits.len() {
-        process_digits(ndigits, &common_prefix, &mut candidates);
+        if ndigits.is_multiple_of(2) {
+            process_digits(ndigits, 2, &common_prefix, &mut candidates);
+        }
         ndigits += 1;
     }
     candidates
@@ -100,17 +123,45 @@ fn process_range(range: &Range) -> Vec<u64> {
         .collect()
 }
 
+fn p2_process(range: &NumRange) -> Vec<u64> {
+    let common_prefix: Vec<char> = range.collect_prefix();
+    let mut candidates = Vec::<_>::new();
+    let mut ndigits = range.lo_digits.len();
+    while ndigits <= range.hi_digits.len() {
+        for i in factors(ndigits) {
+            if i > 1 {
+                process_digits(ndigits, i, &common_prefix, &mut candidates);
+            }
+        }
+        ndigits += 1;
+    }
+    candidates
+        .into_iter()
+        .filter_map(|v| {
+            let val: u64 = v.iter().collect::<String>().parse().unwrap();
+            if val >= range.lo && val <= range.hi {
+                Some(val)
+            } else {
+                None
+            }
+        })
+        .unique()
+        .collect()
+}
+
 fn main() {
     let argv: Vec<String> = env::args().collect();
     if argv.len() != 2 {
         panic!("USAGE: {} <input.dat>", argv[0]);
     }
     let contents = fs::read_to_string(&argv[1]).expect("unable to read file");
-    let ranges: Vec<Range> = contents.split(',').map(Range::parse).collect();
+    let ranges: Vec<NumRange> = contents.split(',').map(NumRange::parse).collect();
     let mut p1_score = 0;
-    let p2_score: i32 = 0;
+    let mut p2_score = 0;
     for range in ranges {
-        p1_score += process_range(&range).iter().sum::<u64>();
+        // println!("{}-{}", range.lo, range.hi);
+        p1_score += p1_process(&range).iter().sum::<u64>();
+        p2_score += p2_process(&range).iter().sum::<u64>();
     }
     println!("part1: {}", p1_score);
     println!("part2: {}", p2_score);
@@ -122,25 +173,71 @@ mod tester {
 
     #[test]
     fn prefix_test() {
-        let r = Range::parse("123456-123567");
+        let r = NumRange::parse("123456-123567");
         assert_eq!(r.collect_prefix(), "123".chars().collect::<Vec<_>>());
     }
 
     #[test]
     fn p1_process_test() {
-        assert_eq!(process_range(&Range::parse("11-22")), vec![11, 22]);
-        assert_eq!(process_range(&Range::parse("99-115")), vec![99]);
-        assert_eq!(process_range(&Range::parse("998-1012")), vec![1010]);
+        assert_eq!(p1_process(&NumRange::parse("11-22")), vec![11, 22]);
+        assert_eq!(p1_process(&NumRange::parse("99-115")), vec![99]);
+        assert_eq!(p1_process(&NumRange::parse("998-1012")), vec![1010]);
         assert_eq!(
-            process_range(&Range::parse("1188511880-1188511890")),
+            p1_process(&NumRange::parse("1188511880-1188511890")),
             vec![1188511885]
         );
-        assert_eq!(process_range(&Range::parse("222220-222224")), vec![222222]);
-        assert_eq!(process_range(&Range::parse("1698522-1698528")), vec![]);
-        assert_eq!(process_range(&Range::parse("446443-446449")), vec![446446]);
+        assert_eq!(p1_process(&NumRange::parse("222220-222224")), vec![222222]);
+        assert_eq!(p1_process(&NumRange::parse("1698522-1698528")), vec![]);
+        assert_eq!(p1_process(&NumRange::parse("446443-446449")), vec![446446]);
         assert_eq!(
-            process_range(&Range::parse("38593856-38593862")),
+            p1_process(&NumRange::parse("38593856-38593862")),
             vec![38593859]
         );
+    }
+
+    #[test]
+    fn p2_process_test() {
+        assert_eq!(
+            p2_process(&NumRange::parse("2727216511-2727316897")),
+            vec![2727227272, 2727272727]
+        );
+        assert_eq!(p2_process(&NumRange::parse("1-22")), vec![11, 22]);
+        assert_eq!(p2_process(&NumRange::parse("11-22")), vec![11, 22]);
+        assert_eq!(p2_process(&NumRange::parse("99-115")), vec![99, 111]);
+        assert_eq!(p2_process(&NumRange::parse("998-1012")), vec![999, 1010]);
+        assert_eq!(
+            p2_process(&NumRange::parse("1188511880-1188511890")),
+            vec![1188511885]
+        );
+        assert_eq!(p2_process(&NumRange::parse("222220-222224")), vec![222222]);
+        assert_eq!(p2_process(&NumRange::parse("1698522-1698528")), vec![]);
+        assert_eq!(p2_process(&NumRange::parse("446443-446449")), vec![446446]);
+        assert_eq!(
+            p2_process(&NumRange::parse("38593856-38593862")),
+            vec![38593859]
+        );
+        assert_eq!(p2_process(&NumRange::parse("565653-565659")), vec![565656]);
+        assert_eq!(
+            p2_process(&NumRange::parse("824824821-824824827")),
+            vec![824824824]
+        );
+        assert_eq!(
+            p2_process(&NumRange::parse("2121212118-2121212124")),
+            vec![2121212121]
+        );
+    }
+
+    #[test]
+    fn factors_test() {
+        assert_eq!(factors(1), vec![1]);
+        assert_eq!(factors(2), vec![2]);
+        assert_eq!(factors(3), vec![3]);
+        assert_eq!(factors(4), vec![2, 4]);
+        assert_eq!(factors(5), vec![5]);
+        assert_eq!(factors(6), vec![2, 3, 6]);
+        assert_eq!(factors(7), vec![7]);
+        assert_eq!(factors(8), vec![2, 4, 8]);
+        assert_eq!(factors(9), vec![3, 9]);
+        assert_eq!(factors(10), vec![2, 5, 10]);
     }
 }
---
+++ 2025/rust/src/bin/day03.rs
@@ -0,0 +1,100 @@
+use std::{env, fs};
+
+fn to_int(chars: &[char]) -> u64 {
+    let mut result = 0;
+    let mut mult = 1;
+    let zero = '0' as u64;
+    for c in chars.iter().rev() {
+        result += (*c as u64 - zero) * mult;
+        mult *= 10;
+    }
+    result
+}
+
+fn largest_pair(line_str: &str) -> u64 {
+    let line: Vec<_> = line_str.chars().collect();
+    let mut first = None;
+    let mut second = None;
+    for i in 0..line.len() {
+        let next = line[i];
+        if let Some(cur) = first {
+            if next > cur && i != line.len() - 1 {
+                first = Some(next);
+                second = None;
+                continue;
+            }
+        } else {
+            first = Some(next);
+            continue;
+        }
+        if let Some(cur) = second {
+            if next < cur {
+                continue;
+            }
+        }
+        second = Some(next);
+    }
+    to_int(&[first.unwrap(), second.unwrap()])
+}
+
+fn largest(line: &[char], ndigits: usize, result: &mut Vec<char>) {
+    if 0 == ndigits {
+        return;
+    }
+    assert!(line.len() >= ndigits);
+    let usable_len = line.len() - ndigits + 1;
+    let mut idx = None;
+    for i in 0..usable_len {
+        if let Some(max_idx) = idx {
+            if line[i] <= line[max_idx] {
+                continue;
+            }
+        }
+        idx = Some(i);
+    }
+    let pos = idx.expect("empty line");
+    result.push(line[pos]);
+    let next_line = &line[pos + 1..];
+    largest(next_line, ndigits - 1, result);
+}
+
+fn largest_12(line_str: &str) -> u64 {
+    let line: Vec<_> = line_str.chars().collect();
+    let mut result = Vec::<char>::with_capacity(12);
+    largest(&line, 12, &mut result);
+    to_int(&result)
+}
+
+fn main() {
+    let argv: Vec<_> = env::args().collect();
+    if argv.len() != 2 {
+        panic!("USAGE: {} <input.dat>", argv[0]);
+    }
+    let contents = fs::read_to_string(&argv[1]).expect("unable to read file");
+    let lines: Vec<_> = contents.split("\n").filter(|l| l.len() > 0).collect();
+    let p1: u64 = lines.clone().into_iter().map(largest_pair).sum();
+    println!("part1: {}", p1);
+    let p2: u64 = lines.into_iter().map(largest_12).sum();
+    println!("part2: {}", p2);
+}
+
+#[cfg(test)]
+mod tester {
+    use super::*;
+
+    #[test]
+    fn largest_pair_test() {
+        assert_eq!(largest_pair("987654321111111"), 98);
+        assert_eq!(largest_pair("811111111111119"), 89);
+        assert_eq!(largest_pair("234234234234278"), 78);
+        assert_eq!(largest_pair("818181911112111"), 92);
+    }
+
+    #[test]
+    fn largest_12_test() {
+        assert_eq!(largest_12("987654321111111"), 987654321111);
+        assert_eq!(largest_12("811111111111119"), 811111111119);
+        assert_eq!(largest_12("234234234234278"), 434234234278);
+        assert_eq!(largest_12("818181911112111"), 888911112111);
+    }
+}
