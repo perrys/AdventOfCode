d192a4d501cebec152e06b909363c2026cf20c62
Stu Perry <stewart.c.perry@gmail.com>
2025-12-10 08:16:55 +0000

    working day8 p1

--- 2025/rust/src/bin/day08.rs
+++ 2025/rust/src/bin/day08.rs
@@ -1,14 +1,15 @@
+use std::collections::HashSet;
+
 fn main() {
-    // let argv = std::env::args().collect::<Vec<_>>();
-    // if argv.len() != 2 {
-    //     panic!("USAGE: {} <input.dat>", argv[0]);
-    // }
-    //let contents = std::fs::read_to_string(&argv[1]).expect("invalid filename");
-    let contents = std::fs::read_to_string("/tmp/foo.dat").expect("invalid filename");
+    let argv = std::env::args().collect::<Vec<_>>();
+    if argv.len() != 2 {
+        panic!("USAGE: {} <input.dat>", argv[0]);
+    }
+    let contents = std::fs::read_to_string(&argv[1]).expect("invalid filename");
     println!("part1: {}", part1(&contents));
 }
 
-type Point = (i32, i32, i32);
+type Point = (i64, i64, i64);
 
 #[derive(PartialEq, PartialOrd, Debug)]
 struct PointPair {
@@ -38,17 +39,17 @@ fn parse(contents: &str) -> Vec<Point> {
             let p1 = iter
                 .next()
                 .expect("1st point")
-                .parse::<i32>()
+                .parse::<i64>()
                 .expect("non-integer");
             let p2 = iter
                 .next()
                 .expect("2nd point")
-                .parse::<i32>()
+                .parse::<i64>()
                 .expect("non-integer");
             let p3 = iter
                 .next()
                 .expect("3rd point")
-                .parse::<i32>()
+                .parse::<i64>()
                 .expect("non-integer");
             (p1, p2, p3)
         })
@@ -60,7 +61,7 @@ fn part1(contents: &str) -> usize {
     let n = points.len();
     let mut distances = Vec::with_capacity(n / 2 * (n + 1));
     for i in 0..points.len() {
-        for j in i..points.len() {
+        for j in (i + 1)..points.len() {
             distances.push(PointPair::new(&points[i], &points[j]));
         }
     }
@@ -69,32 +70,47 @@ fn part1(contents: &str) -> usize {
             .partial_cmp(&rhs.distance)
             .expect("unexpected NaN!")
     });
-    distances.reverse();
-    println!("{distances:?}");
+    // for d in distances.iter() {
+    //     println!("{d:?}");
+    // }
 
-    let mut groups = Vec::<Vec<_>>::new();
-    for pair in distances {
-        let mut found = false;
+    let mut groups = Vec::<HashSet<Point>>::new();
+    let sample_size = 1000;
+    for PointPair {
+        distance: _,
+        points,
+    } in distances.iter().take(sample_size)
+    {
+        let mut group_idxs = (None, None);
         for i in 0..groups.len() {
-            if groups[i].iter().any(|other_pair: &(Point, Point)| {
-                other_pair.0 == pair.points.0
-                    || other_pair.1 == pair.points.1
-                    || other_pair.0 == pair.points.1
-                    || other_pair.1 == pair.points.0
-            }) {
-                groups[i].push(pair.points);
-                found = true;
-                break;
+            if groups[i].contains(&points.0) {
+                group_idxs.0 = Some(i);
+            }
+            if groups[i].contains(&points.1) {
+                group_idxs.1 = Some(i);
             }
         }
-        if !found {
-            groups.push(vec![pair.points]);
+        if let Some(i) = group_idxs.0
+            && let Some(j) = group_idxs.1
+        {
+            if i != j {
+                let last = i.max(j);
+                let first = i.min(j);
+                let tmp = groups.remove(last);
+                groups[first].extend(tmp);
+            }
+        } else if let Some(i) = group_idxs.0 {
+            groups[i].insert(points.1);
+        } else if let Some(i) = group_idxs.1 {
+            groups[i].insert(points.0);
+        } else {
+            groups.push(HashSet::from([points.0, points.1]));
         }
     }
-    println!("{groups:?}");
+    // println!("groups: {groups:?}");
     let mut sizes: Vec<usize> = groups.into_iter().map(|group| group.len()).collect();
-    println!("{sizes:?}");
     sizes.sort();
+    // println!("{sizes:?}");
     sizes
         .into_iter()
         .rev()
